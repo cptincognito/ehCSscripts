2e1c173e5fd82e08326bcb79e15eb75a19377762
Evan Harper <harper4@gmail.com>
2012-08-22 11:14:10 -0400

    Initial Check-In
    Various scripts for Photoshop and After Effects, including normal map
    creation, image appending, and time tunnel effect.

---
+++ createTimeOffsetLayers.jsx
@@ -0,0 +1,40 @@
+// TRACK MATTE MUST BE SET FOR THESE LAYERS BEFORE DUPLICATE
+//in current project/composition
+//get layer 'mask'
+var maskAnim = app.project.item(0).layer(0);
+//get layer 'source'
+var sourceFootage = app.project.item(0).layer(0).duplicate();
+var maxlayers = 256
+var frameDur = (1/24);
+var lastLayer = sourceFootage;
+var lastInpoint = 0;
+var curMaskAnim = null;
+var curSourceFootage = null;
+
+for (i=0, i< maxlayers, i++) {
+	//dup mask to layer (2+i),
+	curMaskAnim = maskAnim.duplicate();
+	curMaskAnim.name = "maskAnim" + i;
+	curMaskAnim.moveAfter(lastLayer);
+	lastLayer = curMaskAnim;
+	curMaskAnim.inpoint = lastInpoint + frameDur;
+	lastPoint = curMaskAnim.inpoint;
+
+	//dup src to layer 3+i
+	curSourceFootage = sourceFootage.duplicate();
+	curSourceFootage.name = "sourceFootage" + i;
+	curSourceFootage.moveAfter(lastLayer);
+	lastLayer = curSourceFootage;
+	curSourceFootage.inpoint = lastInpoint + frameDur;
+	lastPoint = curSourceFootage.inpoint;
+	//apply effect extract to mask
+	//set extract white point i+1
+	//set extract black point i
+   	var theExtractor = curMaskAnim("Effects").addProperty("Extract");
+  	theExtractor(1).setValue(-.04);
+   	theExtractor(2).setValue(4);
+   	theExtractor(3).setValue(1);
+   	theExtractor(4).setValue(.55);
+ 	// OPTIONAL SET SOFTNESS
+}
+
